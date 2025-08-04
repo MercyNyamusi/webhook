c7bca419406acf4dfc0f188b67939b77cefbf3a9
bndambuki123 <bndambuki123@gmail.com>
2025-08-04 17:03:26 +0300

    webhook

--- test.py
+++ test.py
@@ -180,8 +180,8 @@ def create_order():
     
     business_id = data["business_id"]
     vendor = businesses.find_one({"_id": business_id})
-    vendors = vendors.find_one({"_id": vendor})    
-    vendor_id = vendors["_id"]
+    vendors_list = vendors.find_one({"_id": vendor})    
+    vendor_id = vendors_list["_id"]
 
     customer = db.customers.find_one({"_id": data["customer_id"]})
     customer_name = customer["name"] if customer else "new customer"
